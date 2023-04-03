bdb18ccf5fe973299baf3a53e020ff21ab679925
Donvine Mugendi <126117678+Donvine254@users.noreply.github.com>
2023-04-03 13:01:43 +0300

    update script.js

--- script.js
+++ script.js
@@ -21,12 +21,26 @@ function autoGrow() {
     setTimeout(sendSupportMessage, 1000);
   }
   
-  function sendSupportMessage() {
-    var supportMessage = "<div class='supportMessage'>Thank you for contacting! Our support team will get back to you shortly.</div>";
-    document.getElementById("chatlog").innerHTML += supportMessage;
-    document.getElementById("userInput").style.height = "50px";
+function sendSupportMessage() {
+  var userInput = document.getElementById("userInput").value.toLowerCase().trim();
+  var supportMessage = "";
+
+  if (userInput.includes("hello") || userInput.includes("hi")) {
+    supportMessage = "<div class='supportMessage'>Hello! How can we assist you today?</div>";
+  } else if (userInput.includes("location") || userInput.includes("located")) {
+    supportMessage = "<div class='supportMessage'>We are located in Nairobi, Kenya.</div>";
+  } else if (userInput.includes("services") || userInput.includes("what do you offer")) {
+    supportMessage = "<div class='supportMessage'>We offer web design, automation, web hosting, domain names, social media management, search engine optimization, and logo design.</div>";
+  } else if (userInput.includes("open") || userInput.includes("available")) {
+    supportMessage = "<div class='supportMessage'>We are open from Monday to Friday, 8 AM to 4 PM.</div>";
+  } else {
+    supportMessage = "<div class='supportMessage'>Thanks for your message! Our support team will get back to you shortly.</div>";
   }
-  
+
+  document.getElementById("chatlog").innerHTML += supportMessage;
+  document.getElementById("userInput").style.height = "50px";
+}
+
   document.addEventListener('DOMContentLoaded', function() {
     var userInput = document.getElementById("userInput");
     userInput.addEventListener("keydown", function(event) {
