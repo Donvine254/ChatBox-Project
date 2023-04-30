50546bb819af3cb6f2561738c06169dbc3baedb3
donvine mugendi <+>
2023-05-01 02:29:22 +0300

    updated index.js

--- script.js
+++ script.js
@@ -22,36 +22,25 @@ function sendUserInput(event) {
 }
 
 function sendSupportMessage() {
-  var userInput = document.getElementById("userInput").value.toLowerCase().trim();
-  var supportMessage = "";
-
-  switch (true) {
-    case userInput.includes("hello") || userInput.includes("hi"):
-      supportMessage = "<div class='supportMessage'>Hello! How can we assist you today?</div>";
-      break;
-    case userInput.includes("location") || userInput.includes("located"):
-      supportMessage = "<div class='supportMessage'>We are located in Nairobi, Kenya.</div>";
-      break;
-    case userInput.includes("services") || userInput.includes("what do you offer"):
-      supportMessage = "<div class='supportMessage'>We offer web design, automation, web hosting, domain names, social media management, search engine optimization, and logo design.</div>";
-      break;
-    case userInput.includes("open") || userInput.includes("available"):
-      supportMessage = "<div class='supportMessage'>We are open from Monday to Friday, 8 AM to 4 PM.</div>";
-      break;
-    default:
-      supportMessage = "<div class='supportMessage'>Thanks for your message! Our support team will get back to you shortly.</div>";
-      break;
-  }
-
+  let userInput = document.getElementById("userInput");
+  let supportMessage = "";
+  console.log(userInput.value.toLowerCase().trim());
+  supportMessage = "<div class='supportMessage'>Thanks for your message! Our support team will get back to you shortly.</div>";
   document.getElementById("chatlog").innerHTML += supportMessage;
   document.getElementById("userInput").style.height = "50px";
-}
+  }
+
+  
 
 document.addEventListener("DOMContentLoaded", function () {
   let userInput = document.getElementById("userInput");
   userInput.addEventListener("keydown", function (event) {
-    if (event.keyCode === 13) {
+    if (event.key === "Enter") {
       sendUserInput(event);
     }
+  });
+  const chatBtn = document.getElementById("chat");
+  chatBtn.addEventListener("click", function () {
+    sendUserInput();
   });
 });
